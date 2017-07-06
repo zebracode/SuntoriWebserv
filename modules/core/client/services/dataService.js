c96bc7e917b368b46312a399dda3018cf48f3cf5
ZebraCode <info@zebra-code.com>
2017-07-06 21:40:24 +0700

    Revert

--- modules/core/client/services/dataService.js
+++ modules/core/client/services/dataService.js
@@ -16,27 +16,27 @@ angular.module('core').service('usersService', ['$q',
         {
             name: 'หน้าหลัก',
             avatar: 'logo',
-            content: ''
+            content: 'รายละเอียดการใช้งานในหน้าหลัก'
         },
         {
             name: 'ประวัติการใช้งาน',
             avatar: 'logo',
-            content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
+            content: 'รายละเอียดการใช้งานในหน้าประวัติการใช้งาน'
         },
         {
             name: 'เพิ่มรายการ',
             avatar: 'logo',
-            content: 'Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeneys blog. Try-hard art party Shoreditch selfies. Odd Future butcher VHS, disrupt pop-up Thundercats chillwave vinyl jean shorts taxidermy master cleanse letterpress Wes Anderson mustache Helvetica. Schlitz bicycle rights chillwave irony lumberhungry Kickstarter next level sriracha typewriter Intelligentsia, migas kogi heirloom tousled. Disrupt 3 wolf moon lomo four loko. Pug mlkshk fanny pack literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS.'
+            content: 'รายละเอียดการใช้งานในหน้าเพิ่มรายการ'
         },
         {
             name: 'รายชื่อผู้รับ',
             avatar: 'logo',
-            content: 'Scratch the furniture spit up on light gray carpet instead of adjacent linoleum so eat a plant, kill a hand pelt around the house and up and down stairs chasing phantoms run in circles, or claw drapes. Always hungry pelt around the house and up and down stairs chasing phantoms.'
+            content: 'รายละเอียการใช้งานในหน้ารายชื่อผู้รับ'
         },
         {
             name: 'รายชื่อผู้ส่ง',
             avatar: 'logo',
-            content: 'Webtwo ipsum dolor sit amet, eskobo chumby doostang bebo. Bubbli greplin stypi prezi mzinga heroku wakoopa, shopify airbnb dogster dopplr gooru jumo, reddit plickers edmodo stypi zillow etsy.'
+            content: 'รายละเอียดการใช้งานในหน้ารายชื่อผู้ส่ง'
         }
     ];
 
