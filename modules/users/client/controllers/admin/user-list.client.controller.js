be5233cf8eb4efa1d95499fed3c515f729fa6b75
Boonchuay <prototype.2530@gmail.com>
2017-08-30 09:32:33 +0700

    Topup

--- modules/users/client/controllers/admin/user-list.client.controller.js
+++ modules/users/client/controllers/admin/user-list.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('users.admin').controller('UserListController', ['$scope', '$filter', 'Admin', '$mdDialog',
-  function ($scope, $filter, Admin, $mdDialog) {
+angular.module('users.admin').controller('UserListController', ['$scope', '$filter', 'Admin', '$mdDialog', '$http',
+  function ($scope, $filter, Admin, $mdDialog, $http) {
     Admin.query(function (data) {
       $scope.users = data;
       $scope.buildPager();
@@ -28,7 +28,7 @@ angular.module('users.admin').controller('UserListController', ['$scope', '$filt
       $scope.figureOutItemsToDisplay();
     };
 
-    $scope.showTopUpPromt = function(event) {
+    $scope.showTopUpPromt = function(event, user, index) {
         // Appending dialog to document.body to cover sidenav in docs app
         var confirm = $mdDialog.prompt()
           .title('เติมเงิน')
@@ -42,9 +42,40 @@ angular.module('users.admin').controller('UserListController', ['$scope', '$filt
 
           $mdDialog.show(confirm).then(function(result) {
             $scope.status = 'You decided to name your dog ' + result + '.';
+            $scope.updateBalance(user, index, result);
           }, function() {
             $scope.status = 'You didn\'t name your dog.';
-          });
-        }
+        });
+    };
+    
+    $scope.updateBalance = function (user, index, amt) {
+      var balanceAmt = parseInt($scope.pagedItems[index].balanceAmt);
+      console.log(balanceAmt);
+      if (balanceAmt > 0){
+        console.log("if");
+        balanceAmt += parseInt(amt);
+        var data = { userId: user._id, balanceAmt: balanceAmt};
+        $scope.pagedItems[index].balanceAmt = balanceAmt;
+        $http.put('/api/balance',data).then(function(){
+        });
+        
+      } else {
+        console.log("else");
+        $scope.pagedItems[index].balanceAmt = amt;
+        $http.post('/api/balance',{userId: user._id, balanceAmt: amt}).then(function(){
+        });
+      }
+    };
+    
+    $scope.setBalanceAmt = function(index){
+    console.log("pagedItems: ", index);
+    $http.get("/api/balance?userId=" + $scope.pagedItems[index]._id)
+        .then(function(res) {
+           if (res.data !== null) {
+               $scope.pagedItems[index].balanceAmt = res.data.balanceAmt;
+           }
+        });
+    };
+
   }
 ]);
