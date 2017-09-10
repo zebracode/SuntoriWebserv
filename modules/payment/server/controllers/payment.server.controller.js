6c12e9b56448867c6b3c77182b3140b1ec4b6792
Boonchuay <prototype.2530@gmail.com>
2017-09-11 00:12:01 +0700

    list & create Document Number

--- modules/payment/server/controllers/payment.server.controller.js
+++ modules/payment/server/controllers/payment.server.controller.js
@@ -1,9 +1,10 @@
 var CryptoJS = require("crypto-js");
+var DocNo = require('mongoose').model('DocNo');
 
 exports.render = function(req, res) {
     var version = "6.9";
     var merchantId = "764764000000648";
-    var orderId = "ORD2017090700001";
+    var orderId = "ORD00000000000000001";
     var amount = "000000002500";
     
     var stringToHash  = version + merchantId + orderId + amount;
@@ -23,4 +24,25 @@ exports.render = function(req, res) {
     };
     
     res.json(result);
+};
+
+exports.createDocNo = function(req, res, next) {
+    var docNo = new DocNo(req.body);
+    docNo.save(function(err){
+        if (err) {
+            return next(err);
+        } else {
+            res.json(docNo);
+        }
+    });
+};
+
+exports.listDocNo = function(req, res, next) {
+    DocNo.find({}, function(err, docNo){
+        res.json(docNo);
+    });
+};
+
+exports.updateDocNo = function(req, res, next) {
+    
 };
--- modules/payment/server/routes/payment.server.routes.js
+++ modules/payment/server/routes/payment.server.routes.js
@@ -1,4 +1,7 @@
 module.exports = function(app) {
     var payment = require('../controllers/payment.server.controller');
     app.get('/payment', payment.render);
+    app.route('/docno')
+    .get(payment.listDocNo)
+    .post(payment.createDocNo);
 };
