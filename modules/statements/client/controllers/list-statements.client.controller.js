e69190cfeb7f8728aae38b0da2c05a8f49b8a4fc
prototype.2530@gmail.com <prototype.2530@gmail.com>
2018-06-09 15:27:46 +0700

    If login by Admin role, show all statements of all users.

--- modules/statements/client/controllers/list-statements.client.controller.js
+++ modules/statements/client/controllers/list-statements.client.controller.js
@@ -5,17 +5,44 @@
     .module('statements')
     .controller('StatementsListController', StatementsListController);
 
-  StatementsListController.$inject = ['StatementsService', '$location'];
+  StatementsListController.$inject = ['StatementsService', '$location', 'Authentication'];
 
-  function StatementsListController(StatementsService, $location) {
+  function StatementsListController(StatementsService, $location, Authentication) {
+
     var vm = this;
 
-    vm.statements = StatementsService.query();
+    var isAdmin = false;
+
+    // Check role is admin or not
+    if (Authentication.user){
+      for(var i=0; i<Authentication.user.roles.length; i++){
+        if(Authentication.user.roles[i] === "admin"){
+          isAdmin = true;
+          break;
+        }
+      }
+    }
+
+    if(isAdmin) {
+      vm.statements = StatementsService.query();
+    } else {
+      vm.statements = StatementsService.query(
+        function(response){
+          var userStatements = [];
+          for(var i=0; i<response.length; i++){
+            if(response[i].user._id === Authentication.user._id){
+              userStatements.push(vm.statements[i]);
+            }
+          }
+          vm.statements = userStatements;
+        }
+      );
+    }
+    
     vm.exportExcel = exportExcel;
 
     // Export Excel
     function exportExcel() {
-      console.log("Export Excel ...");
       window.location.href = '/api/excel/statements'
     }
 
