d4e0e87777c0dc36be7430b1ef1e18df4b8f444d
Boonchuay <prototype.2530@gmail.com>
2018-08-29 15:46:54 +0700

    Fixed Bug
    1.น้ำหนักหลังส่ง
    2.ชื่อเจ้าของบัญชี
    3.สถานะจัดส่ง

--- modules/mains/server/controllers/mains.server.controller.js
+++ modules/mains/server/controllers/mains.server.controller.js
@@ -470,6 +470,7 @@ exports.exportSummary = function (req, res, next) {
 	}
 
 	Main.find(criteria).sort('-created')
+	.populate('user', 'displayName')
 	.exec(function (err, mains) {
 		if (err) {
 			return res.status(400).send({
@@ -527,7 +528,11 @@ exports.findMains = function(req, res, next){
 		criteria.created = {"$lt": endDate};
 	}
 
-	Main.find(criteria).sort('-created') 
+	// Status
+	criteria.status = {$ne:"ยังไม่ได้ชำระเงิน"};
+
+	Main.find(criteria).sort('-created')
+	.populate('user', 'displayName')
 	.exec(function(err, mains){
 		if(err){
 			return res.status(400).send({
