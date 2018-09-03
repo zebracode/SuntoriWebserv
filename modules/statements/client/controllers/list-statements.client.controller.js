be3940ec6250983351e3b34a2c20ac73530c9213
Boonchuay <prototype.2530@gmail.com>
2018-09-04 00:03:30 +0700

    User - Statements Paging

--- modules/statements/client/controllers/list-statements.client.controller.js
+++ modules/statements/client/controllers/list-statements.client.controller.js
@@ -5,40 +5,31 @@
     .module('statements')
     .controller('StatementsListController', StatementsListController);
 
-  StatementsListController.$inject = ['StatementsService', '$location', 'Authentication'];
+  StatementsListController.$inject = ['StatementsService', '$location', 'Authentication', '$scope'];
 
-  function StatementsListController(StatementsService, $location, Authentication) {
+  function StatementsListController(StatementsService, $location, Authentication, $scope) {
 
     var vm = this;
 
+    $scope.totalItems = 0;
+    $scope.currentPage = 1;
+    $scope.itemsPerPage = 10;
+
     var isAdmin = false;
 
-    // Check role is admin or not
-    if (Authentication.user){
-      for(var i=0; i<Authentication.user.roles.length; i++){
-        if(Authentication.user.roles[i] === "admin"){
-          isAdmin = true;
-          break;
-        }
-      }
-    }
-
-    if(isAdmin) {
-      vm.statements = StatementsService.query();
-    } else {
-      vm.statements = StatementsService.query(
-        function(response){
-          var userStatements = [];
-          for(var i=0; i<response.length; i++){
-            if(response[i].user._id === Authentication.user._id){
-              userStatements.push(vm.statements[i]);
-            }
+    StatementsService.query(
+      function (response) {
+        var userStatements = [];
+        for (var i = 0; i < response.length; i++) {
+          if (response[i].owner._id === Authentication.user._id) {
+            userStatements.push(response[i]);
           }
-          vm.statements = userStatements;
         }
-      );
-    }
-    
+        setPaging(userStatements);
+      }
+    );
+
+
     vm.exportExcel = exportExcel;
 
     // Export Excel
@@ -46,5 +37,37 @@
       window.location.href = '/api/excel/statements';
     }
 
+    $scope.pageChanged = function () {
+      console.log('Page changed to: ' + $scope.currentPage);
+      vm.statements = $scope.allPage[$scope.currentPage];
+    };
+
+    // Page rendering
+    function setPaging(data) {
+      var tempData = [];
+      var pageData = [];
+      var pageIndex = 0;
+      var itemCount = 0;
+      for (var i = 0; i < data.length; i++) {
+        itemCount++;
+        tempData.push(data[i]);
+        if ((itemCount % $scope.itemsPerPage === 0) && (itemCount !== 0)) {
+          pageIndex++;
+          pageData[pageIndex] = tempData;
+          tempData = [];
+        }
+        $scope.totalItems += 1;
+      }
+
+      if (tempData.length > 0) {
+        pageData[pageIndex + 1] = tempData;
+      }
+
+      $scope.allPage = pageData;
+
+      // Set first page
+      // $scope.statements = pageData[1];
+      vm.statements = pageData[1];
+    }
   }
 }());
