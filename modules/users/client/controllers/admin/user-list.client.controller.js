8640858d464d0a6c571d123d5561627cb4d675e0
Boonchuay <prototype.2530@gmail.com>
2018-11-02 00:25:47 +0700

    เรียง list รายการเจ้าของบัญชีจากน้อยไปมาก (a-z)

--- modules/users/client/controllers/admin/user-list.client.controller.js
+++ modules/users/client/controllers/admin/user-list.client.controller.js
@@ -10,8 +10,22 @@ angular.module('users.admin').controller('UserListController', ['$scope', '$filt
 //    $scope.isAdmin = someService.userHasRole('admin');
 
     Admin.query(function (data) {
+      data.sort(
+        function(a,b){
+          var x = a.displayName.toLowerCase();
+          var y = b.displayName.toLowerCase();
+          if (x < y) {
+            return -1;
+          }
+          if (x > y) {
+            return 1;
+          }
+          return 0;
+        }
+      );
       $scope.users = data;
       $scope.allUser = data;
+      console.log($scope.allUser);     
       $scope.buildPager(data);
     });
 
