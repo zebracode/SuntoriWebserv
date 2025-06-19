dd8db2f6f36b8008a1e5c89f79828ed60df7642e
Petcharit Sriwong <info@zebra-code.com>
2025-06-19 23:42:22 +0700

    V0.0.1 | แก้การส่ง api

--- modules/thailandpost/server/controllers/thailandpost.server.controller.js
+++ modules/thailandpost/server/controllers/thailandpost.server.controller.js
@@ -1,18 +1,52 @@
 'use strict';
 
-var fetch = require('node-fetch');
+const fetch = require('node-fetch');
+const mongoose = require('mongoose');
+const TpLastNumber = mongoose.model('TpLastNumber');
 
+// ตรวจสอบสถานะการจัดส่งโดย barcode
 exports.getOrderStatus = function (req, res) {
-    fetch('http://suntoriexpress:suntoriexpressws@r_dservice.thailandpost.com:8080/webservice/getOrderByBarcode?barcode=' + req.query.barcode)
-        .then(function (res) {
-            return res.json();
-        }).then(function (json) {
-            res.send(json);
+    const barcode = req.query.barcode;
+    const url = `http://suntoriexpress:suntoriexpressws@r_dservice.thailandpost.com:8080/webservice/getOrderByBarcode?barcode=${barcode}`;
+
+    fetch(url)
+        .then(async function (response) {
+            const text = await response.text();
+            try {
+                const json = JSON.parse(text);
+                return res.send(json);
+            } catch (err) {
+                console.error(`❌ JSON parse error for barcode ${barcode}:`, err.message);
+                return res.status(502).send({
+                    error: true,
+                    message: 'Invalid JSON response from Thailand Post API',
+                    raw: text
+                });
+            }
+        })
+        .catch(function (error) {
+            console.error(`❌ Fetch failed for barcode ${barcode}:`, error.message);
+            res.status(500).send({
+                error: true,
+                message: 'Fetch error',
+                details: error.message
+            });
         });
 };
 
+// สร้างรายการใหม่
 exports.createOrder = function (req, res) {
-    req.body.productWeight = req.body.productWeight.substring(req.body.productWeight.lastIndexOf('-') + 1, req.body.productWeight.length).replace(/,/g, "");
+    try {
+        req.body.productWeight = req.body.productWeight
+            .substring(req.body.productWeight.lastIndexOf('-') + 1)
+            .replace(/,/g, "");
+    } catch (err) {
+        return res.status(400).send({
+            error: true,
+            message: 'Invalid productWeight format'
+        });
+    }
+
     fetch('http://suntoriexpress:suntoriexpressws@r_dservice.thailandpost.com:8080/webservice/addItem', {
         method: 'POST',
         body: JSON.stringify(req.body),
@@ -20,16 +54,33 @@ exports.createOrder = function (req, res) {
             'Content-Type': 'application/json'
         }
     })
-        .then(function (json) {
-            res.send(json);
+        .then(async function (response) {
+            const text = await response.text();
+            try {
+                const json = JSON.parse(text);
+                return res.send(json);
+            } catch (err) {
+                console.error('❌ JSON parse error in createOrder:', err.message);
+                return res.status(502).send({
+                    error: true,
+                    message: 'Invalid JSON response from Thailand Post API',
+                    raw: text
+                });
+            }
+        })
+        .catch(function (error) {
+            console.error('❌ Fetch failed in createOrder:', error.message);
+            res.status(500).send({
+                error: true,
+                message: 'Fetch error',
+                details: error.message
+            });
         });
 };
 
-var TpLastNumber = require('mongoose').model('TpLastNumber');
-
-// Create last number which use for calculate mod 11
+// สร้างเลขล่าสุด (ใช้สำหรับ mod 11)
 exports.createLastNumber = function (req, res, next) {
-    var tpLastNumber = new TpLastNumber(req.body);
+    const tpLastNumber = new TpLastNumber(req.body);
     tpLastNumber.save(function (err) {
         if (err) {
             return next(err);
@@ -39,7 +90,7 @@ exports.createLastNumber = function (req, res, next) {
     });
 };
 
-// Get last number which use for calculate mod 11
+// ดึงเลขล่าสุด
 exports.getLastNumber = function (req, res, next) {
     TpLastNumber.findOne({}, function (err, tpLastNumbers) {
         if (err) {
@@ -50,16 +101,13 @@ exports.getLastNumber = function (req, res, next) {
     });
 };
 
-
-// Update last number which use for calculate mod 11
+// อัปเดตเลขล่าสุด
 exports.updateLastNumber = function (req, res, next) {
-    TpLastNumber.findOneAndUpdate({}, req.body,
-        function (err, tpLastNumbers) {
-            if (err) {
-                return next(err);
-            } else {
-                res.json(tpLastNumbers);
-            }
+    TpLastNumber.findOneAndUpdate({}, req.body, { new: true }, function (err, tpLastNumbers) {
+        if (err) {
+            return next(err);
+        } else {
+            res.json(tpLastNumbers);
         }
-    );
+    });
 };
